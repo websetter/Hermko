577676f547de8a10789b6994e9b429772ddcebb6
FKauert <f.kauert@werk38.de>
2018-11-08 09:27:44 +0100

    push
    01

---
+++ src/Containers/HermkoItemListContainer1.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hermko\Containers;
+
+use Ceres\Config\CeresConfig;
+use IO\Services\ItemSearch\SearchPresets\TagItems;
+use IO\Services\ItemSearch\Services\ItemSearchService;
+use Plenty\Plugin\Templates\Twig;
+
+class HermkoItemListContainer1
+{
+    public function call(Twig $twig, $arg):string
+    {
+        $tagList = [];
+
+        /** @var CeresConfig $ceresConfig */
+        $ceresConfig = pluginApp(CeresConfig::class);
+
+        $listType = $ceresConfig->itemLists->list1Type;
+
+        if($listType == 'tag_list')
+        {
+            /** @var ItemSearchService $itemSearchService */
+            $itemSearchService = pluginApp( ItemSearchService::class );
+
+            $itemSearchOptions = [
+                'tagIds' => explode(',', $ceresConfig->itemLists->list1TagIds),
+                'sorting' => $ceresConfig->itemLists->tagSorting
+            ];
+
+            $result = $itemSearchService->getResults([
+                                                         'tagItems' => TagItems::getSearchFactory( $itemSearchOptions )
+                                                     ]);
+
+            if(count($result['tagItems']))
+            {
+                $tagList = $result['tagItems']['documents'];
+            }
+        }
+
+        return $twig->render('Hermko::Containers.ItemLists.ItemList1', ["item" => $arg[0], "itemList" => $tagList]);
+    }
+}
